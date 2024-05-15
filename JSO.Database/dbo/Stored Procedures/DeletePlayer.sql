5b2ea5b16913244c2b4b7b5e6a1c75e3dcd92815
Parth Sheth <parthsheth.dcs@gmail.com>
2024-05-15 13:13:22 +0530

    Added DeletePlayer API

---
+++ JSO.Database/dbo/Stored Procedures/DeletePlayer.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].[DeletePlayer] 
+	-- Add the parameters for the stored procedure here
+	@PlayerRegisterId INT,
+	@Success INT OUTPUT
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+	SET @Success = 0
+	IF EXISTS (SELECT 1 FROM PlayerRegister WHERE PlayerRegisterId = @PlayerRegisterId)
+    BEGIN
+		UPDATE PlayerRegister SET IsDeleted = 1 WHERE PlayerRegisterId = @PlayerRegisterId
+		SET @Success = 1;
+	END
+	ELSE
+	BEGIN
+		SET @Success = 0;
+	END
+END
