6512590ab90ddfc00c8ca7b4aadeaf977bf797b1
mayursinh7733 <150129105+cosmosksa@users.noreply.github.com>
2024-07-04 10:58:34 +0530

    DB Changes

---
+++ JSO.Database/dbo/Tables/SignUp.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[SignUp] (
+    [Id]              INT            IDENTITY (1, 1) NOT NULL,
+    [FirstName]       VARCHAR (50)   NULL,
+    [LastName]        VARCHAR (50)   NULL,
+    [EmailId]         NVARCHAR (50)  NULL,
+    [Mobile]          NVARCHAR (50)  NULL,
+    [NewPassword]     NVARCHAR (MAX) NULL,
+    [ConfirmPassword] NVARCHAR (MAX) NULL
+);
+
