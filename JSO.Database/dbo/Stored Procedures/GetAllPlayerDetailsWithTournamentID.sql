2700c8100a95f47a7be04641ea7ab27ae1521536
mayursinh7733 <150129105+cosmosksa@users.noreply.github.com>
2024-07-17 15:11:47 +0530

    DB Changes

--- JSO.Database/dbo/Stored Procedures/GetAllPlayerDetailsWithTournamentID.sql
+++ JSO.Database/dbo/Stored Procedures/GetAllPlayerDetailsWithTournamentID.sql
@@ -3,13 +3,15 @@ CREATE PROCEDURE [dbo].[GetAllPlayerDetailsWithTournamentID]
 @TournamentId int = null
 AS
 BEGIN
-	select PlayerRegisterId, FirstName, LastName, Gender, MobileNo, AlternativePhoneNo, Email, DOB, Batsman, Bowler, WicketKeeper, BattingAllRounder, BowlingAllRounder, PreviousTeamId,
-	LastPlayedYear, ProfilePicture, [Password], BasePrice, WinningBid, T0.UpdatedOn, T0.CreatedOn, T0.CreatedBy, T0.UpdatedBy, IsDeleted, IsActive, T0.City, T0.PlayerCategory, T0.PlayerNo , T0.BattingStyle , T0.BowlingStyle
+	select PlayerRegisterId, FirstName, LastName, Gender, MobileNo, AlternativePhoneNo, T0.Email, DOB, Batsman, Bowler, WicketKeeper, BattingAllRounder, BowlingAllRounder, PreviousTeamId,
+	LastPlayedYear, ProfilePicture, [Password], BasePrice, WinningBid, T0.UpdatedOn, T0.CreatedOn, T0.CreatedBy, T0.UpdatedBy, T0.IsDeleted, T0.IsActive, T0.City, T0.PlayerCategory, T0.PlayerNo , T0.BattingStyle , T0.BowlingStyle , team.TeamName
 	from [dbo].PlayerRegister T0 WITH (NOLOCK)
 	INNER JOIN [dbo].AuctionPlayerMapping T1 WITH (NOLOCK)
 	ON T0.PlayerRegisterId = T1.PlayerId
+	LEFT JOIN [dbo].TeamRegister AS team
+	ON team.TeamId = T0.PreviousTeamId
 	Where 
 	(@TournamentId IS NULL OR @TournamentId = T1.TournamentId) AND
-	IsDeleted = 0
+	T0.IsDeleted = 0
 	ORDER BY T0.CreatedOn DESC
 END
--- JSO.Database/dbo/Tables/SignUp.sql
+++ JSO.Database/dbo/Tables/SignUp.sql
@@ -1,13 +1,16 @@
 ﻿CREATE TABLE [dbo].[SignUp] (
-    [Id]              INT            IDENTITY (1, 1) NOT NULL,
-    [FirstName]       VARCHAR (50)   NULL,
-    [LastName]        VARCHAR (50)   NULL,
-    [EmailId]         NVARCHAR (50)  NULL,
-    [Mobile]          NVARCHAR (50)  NULL,
-    [NewPassword]     NVARCHAR (MAX) NULL,
-    [ConfirmPassword] NVARCHAR (MAX) NULL,
-    [IsDeleted]       BIT            NULL
+    [Id]                 INT              IDENTITY (1, 1) NOT NULL,
+    [FirstName]          VARCHAR (50)     NULL,
+    [LastName]           VARCHAR (50)     NULL,
+    [EmailId]            NVARCHAR (50)    NULL,
+    [Mobile]             NVARCHAR (50)    NULL,
+    [NewPassword]        NVARCHAR (MAX)   NULL,
+    [ConfirmPassword]    NVARCHAR (MAX)   NULL,
+    [IsDeleted]          BIT              NULL,
+    [ResetPasswordToken] UNIQUEIDENTIFIER NULL
 );
+
+
 
 
 
