4c97a4984170c0489d63155f21ce2de401900155
mayurdcs <shahharsh36636@gmail.com>
2025-02-24 14:04:39 +0530

    Resolved the player list issue

--- JSOAuction.Domain.Entities/PlayerRegister/PlayerRegister.cs
+++ JSOAuction.Domain.Entities/PlayerRegister/PlayerRegister.cs
@@ -45,12 +45,12 @@ namespace JSOAuction.Domain.Entities.PlayerRegister
         public int? PlayerGroupId { get; set; }
         public string? PaymentStatus { get; set; }
         public string? IdentityProof { get; set; }
-        public string OccupationDetails { get; set; }
-        public string OccupationTypes { get; set; }
-        public string TShirtNumber { get; set; }
-        public string TShirtName { get; set; }
+        public string? OccupationDetails { get; set; }
+        public string? OccupationTypes { get; set; }
+        public string? TShirtNumber { get; set; }
+        public string? TShirtName { get; set; }
         public decimal? DonationAmount { get; set; }
-        public int TShirtSizeId { get; set; }
+        public int? TShirtSizeId { get; set; }
 
     }
 }
